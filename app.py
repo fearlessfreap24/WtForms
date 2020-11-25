88ed3aaba658f071df2d7ee89195fbbf0c0872b3
fearlessfreap24 <dylanaperez@yahoo.com>
2020-11-25 11:53:59 -0600

    added info for mongodb

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 from LoginForm import LoginForm as lf
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisamajorsecret'
+dbuser = 'FLAKSADMIN'
+dbuserpw = ''
+MONGOURI = f'mongodb+srv://dylan:{dbuserpw}@cluster0.qqxpe.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongo = MongoClient(MONGOURI)
 
 
 @app.route('/form', methods=['GET', 'POST'])
