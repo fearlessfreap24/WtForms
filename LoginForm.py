5aa01f55c3c837887f5d2cbf72a915ee86bf6605
fearlessfreap24 <dylanaperez@yahoo.com>
2020-12-06 06:59:31 -0600

    removed email from login form and page

--- LoginForm.py
+++ LoginForm.py
@@ -5,5 +5,5 @@ from wtforms.validators import InputRequired, Length, Email
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired(),Length(min=5, max=20, message="Must be between 5 and 20 characters")])
-    email = StringField('Email Address', validators=[InputRequired(), Email('An email address is required')])
+    # email = StringField('Email Address', validators=[InputRequired(), Email('An email address is required')])
 
